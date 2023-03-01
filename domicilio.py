125a37fb76c105ad3f53db49425b4c5dea40ff62
mpv24 <125463929+mpv24@users.noreply.github.com>
2023-03-01 11:51:26 -0500

    Add files via upload

---
+++ domicilio.py
@@ -0,0 +1,24 @@
+#Pedir comida a domicilio 
+comida = "perro"
+
+if comida == "perro": 
+    print("El precio del perro es de: 7500")
+    print("Si elegiste esta opcion, tu domicilio sera enviado en 30 minutos")
+elif comida == "hamburguesa":
+    print("El precio de la hamburguesa es de: 8000")
+    print("Si elegiste esta opcion, tu domicilio sera enviado en 30 minutos")
+elif comida == "porcion pizza":
+    print("El precio de la porcion de pizza es de: 6000")
+    print("Si elegiste esta opcion, tu domicilio sera enviado en 45 minutos")
+elif comida == "arroz chino":
+    print("El precio del arroz chino es de: 22500")
+    print("Si elegiste esta opcion, tu domicilio sera enviado en 10 minutos")
+elif comida == "chuzo":
+    print("El precio del chuzo es de: 11000")
+    print("Si elegiste esta opcion, tu domicilio sera enviado en 45 minutos")
+elif comida == "salchipapa":
+    print("El precio de las salchipapas es de: 5500")
+    print("Si elegiste esta opcion, tu domicilio sera enviado en 15 minutos")
+else:
+    print("No se encuentra esta opcion de comida")
+
